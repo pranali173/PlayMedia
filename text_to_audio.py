d9540fa6d78aeaee94b1023c97650b832e8edcea
pranali173 <84122937+pranali173@users.noreply.github.com>
2021-11-09 21:29:27 -0800

    Add files via upload

---
+++ text_to_audio.py
@@ -0,0 +1,13 @@
+from gtts import gtts
+import os
+
+fh = open("test.txt","r")
+myText = fh.read().replace("\n"," ")
+
+language = 'en'
+
+output = gTTS(text=myText,lang=language,slow=False)
+
+output.save("output.mp3")
+fh.close()
+os.system("start output.mp3")
